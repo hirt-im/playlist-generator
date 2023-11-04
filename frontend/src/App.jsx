b3cb515f0722b81264ffc3779ad64c1f4114e8b7
Michael Hirt <hirtm14@gmail.com>
2023-11-04 12:02:11 -0700

    Don't show empty playlist before creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,8 +15,8 @@ function App() {
       <SpotifySignIn />
       <SpotifySignOut />
       <GetAccessToken />
-      <Playlist playlistID={playlistID} />
       <CreatePlaylistForm setPlaylistID={setPlaylistID} />
+      <Playlist playlistID={playlistID} />
     </>
   )
 }
--- frontend/src/Playlist.jsx
+++ frontend/src/Playlist.jsx
@@ -1,8 +1,9 @@
 export default function Playlist({playlistID}){
+    if(!playlistID){return null;}
     return(
         <div>
         <iframe src={`https://open.spotify.com/embed/playlist/${playlistID}?utm_source=generator`}
-                width="300"
+                width="500"
                 height="380"
                 frameBorder="0"
                 allowtransparency="true"
