98429784ec7b596bc286412cfa5d6d2bdb2dd534
Michael Hirt <hirtm14@gmail.com>
2023-11-10 13:20:55 -0800

    Change spacing of directions

--- frontend/src/Directions.jsx
+++ frontend/src/Directions.jsx
@@ -1,4 +1,4 @@
-import { Text, OrderedList, ListItem, Button, Collapse, Heading, Stack } from '@chakra-ui/react';
+import { Text, OrderedList, ListItem, Button, Collapse, Heading, Stack, Flex} from '@chakra-ui/react';
 import './Directions.css';
 import SpotifySignIn from './SpotifySignIn';
 import { useState } from 'react';
@@ -49,13 +49,13 @@ export default function Directions(props){
 
     return(
         <Stack className='intro-container'>
-            <Heading color='white'>SongGuru</Heading>
-            <Text className='directions' pb={2} width='375px' align='left' color='white'>
+            <Heading fontSize={45} color='white'>SongGuru</Heading>
+            <Text className='directions' fontSize={18} pb={2} width='375px' align='left' color='white'>
                 SongGuru utilizes ChatGPT to create a Spotify playlist based on a prompt. The prompt is a theme for your playlist. It can be anything.
                 Use it to make a playlist for a specific event or activity,
                 or to find songs that fit a certain mood or are similar to a different artist. You can be as specific or broad as you like, 
-                the app will take it from there!<br></br> It's simple to use, just: 
-                <OrderedList>
+                the app will take it from there! It's simple to use, just:<br></br>
+                <OrderedList className='list'>
                     {!props.signedIn && <ListItem><SpotifySignIn/></ListItem>}
                     <ListItem>Enter a theme for your playlist</ListItem>
                     <ListItem>Select the number of songs</ListItem>
