2ca0a9d71f96e6bc22cc6c1d84779f40688ecac0
Michael Hirt <hirtm14@gmail.com>
2023-11-20 15:31:07 -0800

    Combine the two buttons into one

--- frontend/src/components/DeletePlaylist.jsx
+++ frontend/src/components/DeletePlaylist.jsx
@@ -2,7 +2,6 @@ import { Button } from '@chakra-ui/react';
 
 
 export default function DeletePlaylist(props){
-
     async function deletePlaylist(){
         try {
             const response = await fetch('http://localhost:3001/deletePlaylist', {
@@ -18,18 +17,10 @@ export default function DeletePlaylist(props){
         }
     }
 
-    if(!props.playlistDeleted){
-        return (
-            <Button width={370} size='lg' colorScheme="red" onClick={deletePlaylist}>
-                Unfollow Playlist
-            </Button>
-        );
-    }
-
-    return(
-        <Button width={370} size='lg' colorScheme="red" isDisabled={true}>
-            Playlist Unfollowed!
+    return (
+        <Button width={370} size='lg' colorScheme="red" onClick={deletePlaylist} isDisabled={props.playlistDeleted}>
+            {(!props.playlistDeleted ? 'Unfollow Playlist' : 'Playlist Unfollowed!')}
         </Button>
-      );
+    );
 };
 
