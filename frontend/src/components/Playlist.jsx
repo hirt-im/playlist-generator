57b0b12d0a1da72249e961d85a3a73c95d4727ec
Michael Hirt <hirtm14@gmail.com>
2023-11-20 15:36:32 -0800

    Remove unnecessary 'return null' statement

--- frontend/src/components/Playlist.jsx
+++ frontend/src/components/Playlist.jsx
@@ -1,15 +1,16 @@
 export default function Playlist({playlistID}){
-    if(!playlistID){return null;}
-    return(
-        <div>
-            <iframe src={`https://open.spotify.com/embed/playlist/${playlistID}?utm_source=generator`}
-                    width="370"
-                    height="480"
-                    frameBorder="0"
-                    allowtransparency="true"
-                    allow="encrypted-media"
-                    title="Spotify Playlist">
-            </iframe>
-        </div>
-    );
+    if(playlistID){
+        return(
+            <div>
+                <iframe src={`https://open.spotify.com/embed/playlist/${playlistID}?utm_source=generator`}
+                        width="370"
+                        height="480"
+                        frameBorder="0"
+                        allowtransparency="true"
+                        allow="encrypted-media"
+                        title="Spotify Playlist">
+                </iframe>
+            </div>
+        );
+    }
 }
