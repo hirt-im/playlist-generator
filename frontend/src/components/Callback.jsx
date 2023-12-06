45fc5bda835f2a77686876b7dd3420bfe84b3b95
Michael Hirt <hirtm14@gmail.com>
2023-12-06 11:58:01 -0800

    Create cookie and go to home page

--- frontend/src/components/Callback.jsx
+++ frontend/src/components/Callback.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import './Callback.css';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Callback(){
     useEffect(() => {
@@ -10,9 +12,20 @@ export default function Callback(){
         .then(res => res.json())
         .then(data => {
             console.log(data);
-        })
+            setCookie(data);
 
+            const navigate = useNavigate();
+            navigate("/");
+        })
     })
+
+
+    function setCookie(accessToken){
+        let date = new Date();
+        date.setTime(date.getTime() + (60 * 60 * 1000));
+        let expires = '; expires=' + date.toUTCString();
+        document.cookie = "access_token=" + accessToken + expires + "; path=/";
+    }
     return(
         <div id='callback'>Callback page</div>
     );
