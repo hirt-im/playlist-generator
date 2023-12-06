0610ec19b03e1952a34f2309c30ffc172466a8f5
Michael Hirt <hirtm14@gmail.com>
2023-12-06 12:35:42 -0800

    Change to hashrouter, change redirect uri

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,19 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import {theme} from './theme.js';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Callback from './components/Callback.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={ <App /> } />
           <Route path="/callback" element={ <Callback /> } />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ChakraProvider>
   </React.StrictMode>,
 )
