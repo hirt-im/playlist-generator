cc924b25aa5ca06e75e92cc6ab8643d59cca9617
Michael Hirt <hirtm14@gmail.com>
2023-12-06 12:48:09 -0800

    Change to browser router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,19 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import {theme} from './theme.js';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Callback from './components/Callback.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={ <App /> } />
           <Route path="/callback" element={ <Callback /> } />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
 )
