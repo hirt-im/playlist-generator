17619667f724b2b03afd9d3a6766d2225f794f60
Michael Hirt <hirtm14@gmail.com>
2023-12-06 11:48:36 -0800

    djlsdf

--- frontend/src/components/Callback.jsx
+++ frontend/src/components/Callback.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import './Callback.css';
 
 export default function Callback(){
+    useEffect(() => {
+        let params = new URLSearchParams(window.location.search);
+        let code = params.get('code');
+
+        fetch('https://songguru.onrender.com/signIn?code=' + code)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+
+    })
     return(
         <div id='callback'>Callback page</div>
     );
