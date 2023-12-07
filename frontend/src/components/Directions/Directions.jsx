cb3dabd20db8ca88f1ea6b1a2f882c229cce757f
Michael Hirt <hirtm14@gmail.com>
2023-12-07 11:57:37 -0800

    Edit description, remove Spotify from page name to avoid problems

--- frontend/src/components/Directions/Directions.jsx
+++ frontend/src/components/Directions/Directions.jsx
@@ -7,12 +7,15 @@ export default function Directions(props){
     return(
         <Stack className='intro-container'>
             <Heading fontSize={45} color='white'>SongGuru</Heading>
-            <Text className='directions' fontSize={18} pb={2} width='375px' align='left' color='white'>
-                SongGuru utilizes ChatGPT to generate a Spotify playlist based on a prompt. The prompt is a theme for your playlist. It can be anything.
+            <Text className='directions' fontSize={18} pb={2} width='375px' align='center' color='white'>
+                {/* SongGuru utilizes ChatGPT to generate a Spotify playlist based on a prompt. The prompt is a theme for your playlist. It can be anything.
                 Use it to make a playlist for a specific event or activity,
                 or to find songs that fit a certain mood or are similar to another artist. You can be as specific or broad as you like, 
-                the app will take it from there! It's simple to use, just:<br></br>
-                <OrderedList ml={20} className='list'>
+                the app will take it from there! It's simple to use, just:<br></br> */}
+                Create a Spotify playlist with ease.
+                
+
+                <OrderedList ml={20} align='left' className='list'>
                     {!props.signedIn && <ListItem><SpotifySignIn/></ListItem>}
                     <ListItem>Enter a theme for your playlist</ListItem>
                     <ListItem>Select the number of songs</ListItem>
